32c55161c33175ac07c07b1ee86ae5f304cb8ebe
Brett Viren <brett.viren@gmail.com>
2019-05-06 22:01:18 -0400

    Initial emacs elisp version, not yet well tested.

---
+++ disproofpoint.el
@@ -0,0 +1,18 @@
+(defun disproofpoint ()
+  (interactive)
+  (save-excursion
+    (let ((restr "\\(https://urldefense\.proofpoint\.com/v[[:digit:]]/url\\?u=\\)\\(.+?\\)\\(&[dcrms]=.*e=\\)")
+          (inhibit-read-only t))
+      (goto-char (point-min))
+      (while (re-search-forward restr nil t)
+        (let* ((url (match-string 2)))
+          (replace-match
+           (save-match-data
+             (url-unhex-string
+              (replace-regexp-in-string
+               "_" "/"
+               (replace-regexp-in-string
+                "-" "%"
+                url))))
+           t t))))))
+
